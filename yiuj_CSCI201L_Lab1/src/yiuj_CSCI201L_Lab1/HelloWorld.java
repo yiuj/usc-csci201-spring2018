3dc3e2e4591f6242c592715bb19f44ec3030968a
yiuj <yiuj@usc.edu>
2018-01-16 17:37:04 -0800

    labs

---
+++ yiuj_CSCI201L_Lab1/src/yiuj_CSCI201L_Lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package yiuj_CSCI201L_Lab1;
+
+public class HelloWorld {
+	public static void main(String []args) {
+		System.out.println("Hello World!");
+//		String s = "Hello World!";
+//		s = null;
+//		s.length();
+	}
+}
---
+++ yiuj_CSCI201L_Lab2/src/yiuj_CSCI201L_Lab2/Lab2.java
@@ -0,0 +1,5 @@
+package yiuj_CSCI201L_Lab2;
+
+public class Lab2 {
+
+}
